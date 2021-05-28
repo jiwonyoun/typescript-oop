1ad9ad747971ab5f943a5a66cdae5be52462a7b6
jiwonyoun <jwy9724@gmail.com>
2021-05-28 15:40:45 +0900

    difference between type and interface

---
+++ 9-advanced/9-1-interface.ts
@@ -0,0 +1,54 @@
+type PositionType = {
+  x: number;
+  y: number;
+};
+
+interface PositionInterface {
+  x: number;
+  y: number;
+}
+
+// object
+const obj1: PositionType = {
+  x: 1,
+  y: 1,
+};
+const obj2: PositionInterface = {
+  x: 1,
+  y: 1,
+  z: 1,
+};
+
+// class
+class Pos1 implements PositionType {
+  x: number;
+  y: number;
+}
+
+class Pos2 implements PositionInterface {
+  x: number;
+  y: number;
+  z: number;
+}
+
+// extends
+type ZPoisitionType = PositionType & { z: number };
+interface ZPositionInterface extends PositionInterface {
+  z: number;
+}
+
+// only interfaces can be merged
+interface PositionInterface {
+  z: number;
+}
+
+// Type aliases can use computed properties
+type Person = {
+  name: string;
+  age: number;
+};
+
+type Name = Person["name"]; // using key by string
+
+type NumberType = number;
+type Direction = "left" | "right";
