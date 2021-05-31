ba6f99eb32f08366e046f231982ebb34fa056897
jiwonyoun <jwy9724@gmail.com>
2021-05-31 11:37:15 +0900

    javascript prototype & this

---
+++ 10-javascript/10-2-this.js
@@ -0,0 +1,26 @@
+console.log(this); // this = Window
+
+function simpleFunc() {
+  console.log(this); // this = Window
+}
+
+simpleFunc();
+
+class Counter {
+  count = 0;
+  increase = () => {
+    console.log(this);
+  };
+}
+const counter = new Counter();
+counter.increase(); // this = Counter
+
+// const caller = counter.increase;
+const caller = counter.increase.bind(counter);
+caller(); // this = undefined
+
+class Bob {}
+
+const bob = new Bob();
+bob.run = counter.increase;
+bob.run();
